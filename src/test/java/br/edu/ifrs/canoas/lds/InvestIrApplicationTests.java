2f5f8cfce9e19ab11ec1864e7f14316dc23d25ac
James <jmtremea@gmail.com>
2017-05-25 03:44:40 -0300

    feat: Implemantação do sistema

---
+++ src/test/java/br/edu/ifrs/canoas/lds/InvestIrApplicationTests.java
@@ -0,0 +1,42 @@
+package br.edu.ifrs.canoas.lds;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.with;
+import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.hook.wait.Wait;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@Wait
+public class InvestIrApplicationTests extends FluentTest{
+
+	
+	@Value("${local.server.port}") 
+	private int port;
+	
+	String getHost(){
+		return "http://localhost:" + port + "/prototipo/index.html";
+	}
+	
+	@Test
+    public void executa_login_no_sistema_e_vai_para_tela_inicial() {
+		//Vai para http://localhost:"+ port+ "/prototipo/index.html
+        goTo(getHost());     
+        //Verifica que o título da página é da aplicação
+        assertThat(window().title()).contains("InvestIR - Amanse o leão");
+        //Clica no botão Login
+        $("#submete_login").click();
+        //Verifica que a página agora tem o nome do usuário logado no topo
+        assertThat($("#nome_usuario").text()).contains("James M. Tremea");
+    }
+
+
+}
